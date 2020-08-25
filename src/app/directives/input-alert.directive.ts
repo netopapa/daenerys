9c0c145c073d96b21ec2203363e565327026dde5
Papa Neto <vspapaneto@gmail.com>
2020-08-24 20:04:15 -0400

    final directive configs

---
+++ src/app/directives/input-alert.directive.ts
@@ -0,0 +1,34 @@
+import { Directive, ElementRef, Input } from '@angular/core';
+import { NgModel } from '@angular/forms';
+
+export interface InputAlertBody {
+  value: any;
+  field: NgModel;
+}
+
+@Directive({
+  selector: '[appInputAlert]'
+})
+export class InputAlertDirective {
+
+  private element: HTMLDivElement;
+
+  constructor(
+    element: ElementRef,
+  ) {
+    this.element = element.nativeElement;
+  }
+
+  @Input() set appInputAlert(e: InputAlertBody) {
+    if (e.field.errors?.required && e.field.dirty) {
+      const alertText: HTMLSpanElement = document.createElement('span');
+      alertText.classList.add('text-alert');
+      alertText.innerText = e.field.name + ' is required!';
+
+      this.element.append(alertText);
+    } else {
+      this.element.querySelector('.text-alert')?.remove();
+    }
+  }
+
+}
