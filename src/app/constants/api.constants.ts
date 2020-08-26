c5262130f68cefdf07ae9965dbd4374b8da5e40c
Papa Neto <vspapaneto@gmail.com>
2020-08-25 21:19:09 -0400

    new login image

--- src/app/constants/api.constants.ts
+++ src/app/constants/api.constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/';
+export const API_URL = 'https://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/';
 export const AUTH_API_URL = 'https://reqres.in/api/login';
 
 export const ENDPOINTS = {
